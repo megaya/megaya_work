0428aa305e7250f17a958920c863d5cb54ff5649
megaya <tomoya.anpan.0403@gmail.com>
2021-06-18 13:12:54 +0900

    2021/06/18

---
+++ javascript_box/vue/20210618/README.md
@@ -0,0 +1,4 @@
+参考
+https://qiita.com/nmgw1119/items/27ed23e433ec54a9c950
+
+
---
+++ javascript_box/vue/20210618/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{ count1 }}
+    {{ count2 }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapState([
+      "count1",
+      "count2"
+    ])
+  }
+}
+</script>
+
